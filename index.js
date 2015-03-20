4965d9e5fa24c51068e8af1225c54fcf7580e77a
David <chronoglass@gmail.com>
2015-03-20 11:33:34 -0700

    more cleanup

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var main = require('./routes/main')
+var express = require('express');
+var app = express();
+var main = require('./routes/main');
 
-app.use('main', main)
+app.use('main', main);
+
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://%s:%s', host, port);
+});
